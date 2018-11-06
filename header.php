535c597fa261691732f1f5b3b5e51df048a0a9c0
GEN-001 <dcl.quentind@18pixel.fr>
2018-11-06 14:50:32 +0100

    change header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <?php echo '
-<a href="index.html"><img src="./images/HTML5_Logo_256.png" alt="image HTML 5"></a>
+<a href="index.php"><img src="./images/HTML5_Logo_256.png" alt="image HTML 5"></a>
 <h1>WIKI HTML</h1>';
 ?>
