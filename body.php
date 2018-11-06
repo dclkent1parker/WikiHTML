bf57932b7f7ecb6189a83b2bf833751b80475a1d
GEN-001 <dcl.quentind@18pixel.fr>
2018-11-06 15:15:04 +0100

    change body php

--- body.php
+++ body.php
@@ -35,11 +35,7 @@
       </div>
     </main>
     <footer>
-      <p><em>Créé par</em> <strong>les D Codeurs du Lac</strong></p>
-      <img src="./images/LogoLycan256.png" alt="Logo Lycan">
-      <img src="./images/Licorne_mini.png" alt="logo Sylvie">
-      <img src="./images/quentin.png" alt="Logo quentin">
-      <img src="./images/monlogo.jpg" alt="logo black-beard">
+    <?php include ("footer.php"); ?>
     </footer> 
   </body>
 </html>
